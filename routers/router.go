f451278c63afe0ea2b5fbf034aada8b3ab4d5bfb
xiaolong.li <mayli@chinatongyin.com>
2020-07-06 17:50:38 +0800

    add login action

--- routers/router.go
+++ routers/router.go
@@ -1,12 +1,12 @@
 package routers
 
 import (
-	"golang-web-EvanLi/controllers"
 	"github.com/astaxie/beego"
+	"golang-web-EvanLi/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
-	beego.Router("sys_user/login_form",&controllers.SysUserController{},"Get:LoginForm")
-	beego.Router("sys_user/login_form",&controllers.SysUserController{},"Post:LoginAction")
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("sys_user/login_form", &controllers.SysUserController{}, "Get:LoginForm")
+	beego.Router("sys_user/login_form", &controllers.SysUserController{}, "Post:LoginAction")
 }
